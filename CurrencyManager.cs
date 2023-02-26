3580521a2598bad1fca257c29acd137a2f684d4d
Mustafa Salman <frostnote2@gmail.com>
2023-02-26 17:48:58 +0800

    Created the backend logic and functionality to the app

---
+++ CurrencyManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Exchanger
+{
+    class CurrencyManager
+    {
+        public int DollarQty { get; set; }
+        public int FiftyCentQty { get; set; }
+        public int TenCentQty { get; set; }
+        public int FiveCentQty { get; set; }
+
+        public CurrencyManager()
+        {
+            DollarQty = 1;
+            FiftyCentQty = 1;
+            TenCentQty = 1;
+            FiveCentQty = 1;
+        }
+
+        public double TotalCash ()
+        {
+            return Convert.ToDouble(DollarQty + (FiftyCentQty * 0.50) + (TenCentQty * 0.10) + (FiveCentQty * 0.05));
+        }
+        public void AddCurrency(string currency)
+        {
+            switch (currency)
+            {
+                case "dollar":
+                    DollarQty += 1;
+                    break;
+
+                case "fiftyCent":
+                    FiftyCentQty += 1;
+                    break;
+
+                case "tenCent":
+                    TenCentQty += 1;
+                    break;
+
+                case "fiveCent":
+                    FiveCentQty += 1;
+                    break;
+            }
+        }
+
+        public void TakeCurrency(string currency)
+        {
+            switch (currency)
+            {
+                case "dollar":
+                    DollarQty -= 1;
+                    break;
+
+                case "fiftyCent":
+                    FiftyCentQty -= 1;
+                    break;
+
+                case "tenCent":
+                    TenCentQty -= 1;
+                    break;
+
+                case "fiveCent":
+                    FiveCentQty -= 1;
+                    break;
+            }
+        }
+
+        public void DollarTransferDown()
+        {
+            if (DollarQty > 0)
+            {
+                DollarQty -= 1;
+                FiftyCentQty += 2; 
+            }
+            else { return; }
+        }
+
+        public void FiftyTransfer(string direction)
+        {
+            if (direction == "up" && FiftyCentQty >= 2)
+            {
+                FiftyCentQty -= 2;
+                DollarQty += 1;
+            }
+            if (direction == "down" && FiftyCentQty >= 1)
+            {
+                FiftyCentQty -= 1;
+                TenCentQty += 5;
+            }
+            else { return; }
+        }
+
+
+        public void TenTransfer(string direction)
+        {
+            if (direction == "up" && TenCentQty >= 5)
+            {
+                TenCentQty -= 5;
+                FiftyCentQty += 1;
+            }
+            if (direction == "down" && TenCentQty >= 1)
+            {
+                TenCentQty -= 1;
+                FiveCentQty += 2;
+            }
+            else { return; }
+        }
+
+        public void FiveTransferUp()
+        {
+            if (FiveCentQty >= 2)
+            {
+                FiveCentQty -= 2;
+                TenCentQty += 1;
+            }
+            else { return; }
+        }
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,117 @@
-﻿namespace Currency_Exchanger;
+﻿using System.Globalization;
+
+namespace Currency_Exchanger;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	CurrencyManager manager = new CurrencyManager();
+    CultureInfo usCulture = new CultureInfo("en-US");
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
-	}
+		Refresh();
+    }
+
+	private void Refresh()
+	{
+        TotalLabel.Text = $"{manager.TotalCash().ToString("C2", usCulture)}";
+
+		DollarQtyLabel.Text = $"$1\nx{manager.DollarQty}";
+		DollarTotalLabel.Text = $"{manager.DollarQty.ToString("C2", usCulture)}";
+
+		FiftyQtyLabel.Text = $"50c\nx{manager.FiftyCentQty}";
+		FiftyTotalLabel.Text = $"{Convert.ToDouble(manager.FiftyCentQty * 0.50).ToString("C2", usCulture)}";
+
+		TenQtyLabel.Text = $"10c\nx{manager.TenCentQty}";
+		TenTotalLabel.Text = $"{Convert.ToDouble(manager.TenCentQty * 0.10).ToString("C2", usCulture)}";
+
+		FiveQtyLabel.Text = $"5c\nx{manager.FiveCentQty}";
+		FiveTotalLabel.Text = $"{Convert.ToDouble(manager.FiveCentQty * 0.05).ToString("C2", usCulture)}";
+    }
+
+    private void DollarAddButton_Pressed(object sender, EventArgs e)
+    {
+        manager.AddCurrency("dollar");
+        Refresh();
+    }
+
+    private void DollarTakeButton_Pressed(object sender, EventArgs e)
+    {
+        manager.TakeCurrency("dollar");
+        Refresh();
+    }
+
+    private void DollarToFiftyButton_Pressed(object sender, EventArgs e)
+    {
+        manager.DollarTransferDown();
+        Refresh();
+    }
+
+    private void FiftyAddButton_Pressed(object sender, EventArgs e)
+    {
+        manager.AddCurrency("fiftyCent");
+        Refresh();
+    }
+
+    private void FiftyTakeButton_Pressed(object sender, EventArgs e)
+    {
+        manager.TakeCurrency("fiftyCent");
+        Refresh();
+    }
+
+    private void FiftyToDollarButton_Pressed(object sender, EventArgs e)
+    {
+        manager.FiftyTransfer("up");
+        Refresh();
+    }
+
+    private void FIftyToTenButton_Pressed(object sender, EventArgs e)
+    {
+        manager.FiftyTransfer("down");
+        Refresh();
+    }
+
+    private void TenAddButton_Pressed(object sender, EventArgs e)
+    {
+        manager.AddCurrency("tenCent");
+        Refresh();
+    }
+
+    private void TenTakeButton_Pressed(object sender, EventArgs e)
+    {
+        manager.TakeCurrency("tenCent");
+        Refresh();
+    }
+
+    private void TenToFiftyButton_Pressed(object sender, EventArgs e)
+    {
+        manager.TenTransfer("up");
+        Refresh();
+    }
+
+    private void TenToFiveButton_Pressed(object sender, EventArgs e)
+    {
+        manager.TenTransfer("down");
+        Refresh();
+    }
+
+    private void FiveAddButton_Pressed(object sender, EventArgs e)
+    {
+        manager.AddCurrency("fiveCent");
+        Refresh();
+    }
+
+    private void FiveTakeButton_Pressed(object sender, EventArgs e)
+    {
+        manager.TakeCurrency("fiveCent");
+        Refresh();
+    }
+
+    private void FiveToTenButton_Pressed(object sender, EventArgs e)
+    {
+        manager.FiveTransferUp();
+        Refresh();
+    }
 }
 
